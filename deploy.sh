d41d164a4483a775dcb6d3b301ff1e892166e5dc
nryberg <nryberg@gmail.com>
2026-01-19 11:16:47 -0600

    Update deploy.sh
    Fixing settings

--- deploy.sh
+++ deploy.sh
@@ -4,24 +4,37 @@
 # Pushes the stats page to the Raspberry Pi
 
 SERVER="framboise"
-USER="pi"
-REMOTE_DIR="/home/pi/mypistats"
+USER="nick"
+REMOTE_DIR="/home/nick/mypistats"
+AUTOSTART_DIR="/home/nick/.config/autostart"
 
 echo "Deploying to $SERVER..."
 
-# Create remote directory if it doesn't exist
-ssh "$USER@$SERVER" "mkdir -p $REMOTE_DIR"
+# Create remote directories if they don't exist
+ssh "$USER@$SERVER" "mkdir -p $REMOTE_DIR $AUTOSTART_DIR"
 
 # Copy files
 scp index.html "$USER@$SERVER:$REMOTE_DIR/"
 
-if [ $? -eq 0 ]; then
-    echo "Deploy successful!"
-    echo "Files deployed to $USER@$SERVER:$REMOTE_DIR"
-    echo ""
-    echo "To view, run on the Pi:"
-    echo "  chromium-browser --kiosk $REMOTE_DIR/index.html"
-else
+if [ $? -ne 0 ]; then
     echo "Deploy failed!"
     exit 1
 fi
+
+# Create autostart entry for kiosk mode on boot
+ssh "$USER@$SERVER" "cat > $AUTOSTART_DIR/mypistats.desktop << 'EOF'
+[Desktop Entry]
+Type=Application
+Name=MyPiStats
+Exec=env DISPLAY=:0 chromium --kiosk --start-fullscreen --noerrdialogs --disable-infobars --no-first-run --check-for-update-interval=31536000 --disable-session-crashed-bubble --disable-gpu-vsync --disable-software-rasterizer $REMOTE_DIR/index.html
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+EOF"
+
+echo "Deploy successful!"
+echo "Files deployed to $USER@$SERVER:$REMOTE_DIR"
+echo "Autostart configured - will launch on reboot"
+echo ""
+echo "To view now, run on the Pi (via SSH):"
+echo "  DISPLAY=:0 chromium --kiosk --start-fullscreen --disable-gpu-vsync $REMOTE_DIR/index.html"
